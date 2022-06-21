e1994dd36e997d0c75d7d65f85b62b95d8b1bb79
51eating <21963949+dnjstlr555@users.noreply.github.com>
2022-06-21 19:12:51 +0900

    delete

--- test.js
+++
@@ -1,9 +0,0 @@
-const bbs = require('./bbs.js')
-const board = new bbs('posting.db')
-
-board.WriteComment("이거 좋네요 ㅋㅋㄹㅋㅋ",1234,8,0)
-board.WriteComment("와 진짜 전설이다...",1234,8,0)
-board.WriteComment("ㄹㅇㅋㅋ",1234,8,0)
-
-board.WriteComment("안녕하세요 무플방지위원회",1234,9,0)
-board.WriteComment("ㅋㅋㄹㅃㅃ",1234,9,0)
